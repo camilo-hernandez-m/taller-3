30d226a755ad02daeb116b2f4b916193c9bdec6d
camilohm1 <cahernandez3510@misena.edu.co>
2022-10-10 11:26:41 -0500

    punto1

---
+++ punto1.js
@@ -0,0 +1,13 @@
+addEventListener("DOMContentLoaded", ()=>{
+   let numero = Number(prompt("ingrese un numero:"))
+   if (numero > 100 &&numero>0){
+      console.log(`el numero :${numero} era mayor que 100`);
+      console.log(`el numero :${numero} era mayor que 100`)
+   }else{
+      console.log(`era menor que 100`)
+      console.log(`el numero esta entre 100`)
+   }
+
+
+
+})
